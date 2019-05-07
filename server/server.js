0bdb0142d4ea0fb3cb6a3fdcc7ffeec55a7d5e93
Rommel Porras <rporras@yondu.com>
2019-05-07 21:12:03 +0800

    Add geolocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const app = express();
@@ -23,29 +23,11 @@ io.on('connection', (socket) => {
     console.log('createMessage', message);
 
     io.emit('newMessage', generateMessage(message.from, message.text));
-
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
   });
 
-  // socket.emit('newMessage', {
-  //   from: 'John',
-  //   text: 'See you then',
-  //   createdAt: 123123
-  // });
-
-  // socket.emit('newEmail', {
-  //   from: 'mike@example.com',
-  //   text: 'Hey, What is going on.',
-  //   createdAt: 123
-  // });
-
-  // socket.on('createEmail', (newEmail) => {
-  //   console.log('createEmail', newEmail);
-  // });
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+  });
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -12,5 +12,18 @@ describe('generateMessage', () => {
     expect(message).toMatchObject({from, text});
     // expect(message.from).toEqual(from);
     // expect(message.text).toEqual(text);
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    const from = 'Mel';
+    const latitude = 15;
+    const longitude = 19;
+    const url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+    const message = generateLocationMessage(from, latitude, longitude);
+
+    expect(typeof message.createdAt).toBe('number');
+    expect(message).toMatchObject({from, url});
   });
 });
